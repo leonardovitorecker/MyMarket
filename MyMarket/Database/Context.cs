ac37742f6b3aec13c3263ffb8322ea301adab66e
leonardo ecker <leonardovitoecker@gmail.com>
2022-10-19 19:47:52 -0300

    3141

--- MyMarket/Database/Context.cs
+++ MyMarket/Database/Context.cs
@@ -5,22 +5,26 @@ namespace MyMarket.Database
 {
     public class Context: DbContext
     {
-            public Context(DbContextOptions<Context> opcoes)
-             : base(opcoes) { }
 
-            protected override void OnModelCreating(ModelBuilder builder)
-            {
-                base.OnModelCreating(builder);
-            }
+        public Context(DbContextOptions<Context> opcoes)
+                 : base(opcoes) { }
 
-            public DbSet<Usuario> usuarios { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+
+        public DbSet<Usuario> usuarios { get; set; }
             public DbSet<Endereco> enderecos { get; set; }
             public DbSet<Produto> produtos { get; set; }
             public DbSet<Categoria> categorias { get; set; }
-            public DbSet<ItemPedidoProduto> itempedidosprodutos { get; set; }
-            public DbSet<Pedido> pedidos { get; set; }
-             
+        public DbSet<Carrinho> Carrinhos { get; set; }
+        public DbSet<Pedido> pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
+    
 
 
-    }
+}
 }
