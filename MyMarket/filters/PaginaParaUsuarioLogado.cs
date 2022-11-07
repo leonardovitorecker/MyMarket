a92d718b070159f4a272164720bd70809158e441
leonardo ecker <leonardovitoecker@gmail.com>
2022-11-07 19:19:36 -0300

    kkkkkk

---
+++ MyMarket/filters/PaginaParaUsuarioLogado.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyMarket.Models;
+using Newtonsoft.Json;
+
+namespace MyMarket.filters
+{
+    public class PaginaParaUsuarioLogado : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string sessaoUsuario = context.HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(sessaoUsuario))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+            }
+            else
+            {
+                Usuario usuario = JsonConvert.DeserializeObject<Usuario>(sessaoUsuario);
+
+                if (usuario == null)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                }
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ MyMarket/viewComponents/Menu.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MyMarket.Models;
+using Newtonsoft.Json;
+namespace MyMarket.viewComponents
+{
+    public class Menu : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string sessaoUsuario = HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+           
+            Usuario usuario = JsonConvert.DeserializeObject<Usuario>(sessaoUsuario);
+
+            return View(usuario);
+        }
+    }
+}
